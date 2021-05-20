34494c91c287348e98edcbec1b0963cfc7aac01a
Nikhil Sheoran <51950693+nikhilsheoran@users.noreply.github.com>
2021-05-20 15:08:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function tableFiller(arr) {
     for (let i = 0; i < arr.length; i++) {
         const onecountry = arr[i];
         document.getElementById("tbody").innerHTML += '<tr><th scope="row">' + (i + 1) + '</th><td><a href="template/index.html?country=' + i + '" class="btn btn-outline-danger">' + onecountry['name'] + '</a></td><td>' + onecountry['capital'] + '</td><td>' + onecountry['region'] + '</td></tr>';
-        console.log(arr[i]);
     }
     $('#nametable').DataTable();
     var link = document.querySelector("link[rel~='icon']");
